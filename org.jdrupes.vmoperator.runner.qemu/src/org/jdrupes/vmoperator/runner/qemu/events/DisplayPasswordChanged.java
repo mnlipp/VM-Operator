383d1c5cca134051690d12ff4a7a9746f2df5a41
Michael N. Lipp <mnl@mnl.de>
2024-03-28 16:46:58 +0100

    Reflect display password changes in status.

---
+++ org.jdrupes.vmoperator.runner.qemu/src/org/jdrupes/vmoperator/runner/qemu/events/DisplayPasswordChanged.java
@@ -0,0 +1,39 @@
+/*
+ * VM-Operator
+ * Copyright (C) 2024 Michael N. Lipp
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.jdrupes.vmoperator.runner.qemu.events;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.jdrupes.vmoperator.runner.qemu.commands.QmpCommand;
+
+/**
+ * A {@link MonitorResult} that indicates that the display password has changed.
+ */
+public class DisplayPasswordChanged extends MonitorResult {
+
+    /**
+     * Instantiates a new display password changed.
+     *
+     * @param command the command
+     * @param response the response
+     */
+    public DisplayPasswordChanged(QmpCommand command, JsonNode response) {
+        super(command, response);
+    }
+
+}
