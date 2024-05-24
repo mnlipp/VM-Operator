880cc7172f496eadc213a3f765ee31fae4513475
Michael N. Lipp <mnl@mnl.de>
2024-05-24 16:20:02 +0200

    Basically working version of viewer.

---
+++ org.jdrupes.vmoperator.vmviewer/rollup.config.mjs
@@ -0,0 +1,35 @@
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+
+let packagePath = "org/jdrupes/vmoperator/vmviewer";
+let baseName = "VmViewer"
+let module = "build/generated/resources/" + packagePath 
+    +  "/" + baseName + "-functions.js";
+
+let pathsMap = {
+    "aash-plugin": "../../page-resource/aash-vue-components/lib/aash-vue-components.js",
+    "jgconsole": "../../console-base-resource/jgconsole.js",
+    "jgwc": "../../page-resource/jgwc-vue-components/jgwc-components.js",
+    "l10nBundles": "./" + baseName + "-l10nBundles.ftl.js",
+    "vue": "../../page-resource/vue/vue.esm-browser.js"
+}
+
+export default {
+  external: ['aash-plugin', 'jgconsole', 'jgwc', 'l10nBundles', 'vue', 'chartjs'],
+  input: "src/" + packagePath + "/browser/" + baseName + "-functions.ts",
+  output: [
+    {
+      format: "esm",
+      file: module,
+      sourcemap: true,
+      sourcemapPathTransform: (relativeSourcePath, _sourcemapPath) => {
+        return relativeSourcePath.replace(/^([^/]*\/){12}/, "./");
+      },
+      paths: pathsMap
+    }
+  ],
+  plugins: [
+    typescript(),
+    postcss()
+  ]
+};
