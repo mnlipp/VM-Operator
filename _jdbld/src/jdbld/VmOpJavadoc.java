605eb0aa3068241989c036c4e5963d55550a80a2
Michael N. Lipp <mnl@mnl.de>
2026-08-26 21:31:05 +0200

    Upgrade jdbld.

--- _jdbld/src/jdbld/VmOpJavadoc.java
+++ _jdbld/src/jdbld/VmOpJavadoc.java
@@ -41,7 +41,7 @@ import org.jdrupes.builder.api.ResourceRequest;
 import static org.jdrupes.builder.api.ResourceType.*;
 import org.jdrupes.builder.core.AbstractGenerator;
 import org.jdrupes.builder.java.ClassTree;
-import org.jdrupes.builder.java.ClasspathElement;
+import org.jdrupes.builder.java.CodeContribution;
 import org.jdrupes.builder.java.JarFile;
 import org.jdrupes.builder.java.JavadocDirectory;
 import static org.jdrupes.builder.java.JavaTypes.*;
@@ -91,7 +91,7 @@ public class VmOpJavadoc extends AbstractGenerator implements Renamable {
         // Classpath
         var classpath = elementsToPath(project().rootProject().projects("**")
             .filter(p -> !(p instanceof MergedTestProject))
-            .flatMap(p -> p.resources(of(ClasspathElementType)
+            .flatMap(p -> p.resources(of(CodeContributionType)
                 .using(Consume, Reveal, Expose))));
 
         // Build command
@@ -125,7 +125,7 @@ public class VmOpJavadoc extends AbstractGenerator implements Renamable {
             "-docletpath", elementsToPath(new MvnRepoLookup()
                 .addRepositories(project().get(LookupRepositories)).resolve(
                     "org.jdrupes.mdoclet:doclet:4.2.0")
-                .resources(of(ClasspathElementType).using(Supply, Expose))),
+                .resources(of(CodeContributionType).using(Supply, Expose))),
             "--disable-auto-highlight",
             "-taglet", "org.jdrupes.taglets.plantUml.PlantUml",
             "-taglet", "org.jdrupes.taglets.plantUml.StartUml",
@@ -133,7 +133,7 @@ public class VmOpJavadoc extends AbstractGenerator implements Renamable {
             "-tagletpath", elementsToPath(new MvnRepoLookup()
                 .addRepositories(project().get(LookupRepositories)).resolve(
                     "org.jdrupes.taglets:plantuml-taglet:3.1.0")
-                .resources(of(ClasspathElementType).using(Supply, Expose))),
+                .resources(of(CodeContributionType).using(Supply, Expose))),
             "-overview", project().rootProject()
                 .directory().resolve("overview.md").toString(),
             "-bottom", project().rootProject()
@@ -167,7 +167,7 @@ public class VmOpJavadoc extends AbstractGenerator implements Renamable {
         }
     }
 
-    private String elementsToPath(Stream<ClasspathElement> elements) {
+    private String elementsToPath(Stream<CodeContribution> elements) {
         return elements.<Path> mapMulti((e, consumer) -> {
             if (e instanceof ClassTree classTree) {
                 consumer.accept(classTree.root());
